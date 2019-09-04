33f72e175e70014a4f590dce6a3380b18bb5e0f5
Tin Nguyen <ngutt094@mymail.unisa.edu.au>
2019-09-04 10:45:31 +0930

    Added menu asking if user wants to proceed with transaction. While loops and if statements were used.

--- source/CashRegister.java
+++ source/CashRegister.java
@@ -1,43 +1,63 @@
 import java.util.Scanner;
 
-public class CashRegister
-{
-   public static void main(String[] args)
-   {
-	  String dollar_symbol = "$"; 
-	  
-      String s, c;
-      double balance;
+public class CashRegister {
+	public static void main(String[] args) {
+		String dollar_symbol = "$";
 
-      //comment
-      //Anthony was here.
-      //Test comment 2.
-      
-      Scanner in = new Scanner(System.in);
+		String s, c;
+		char proceed;
+		double balance;
+		boolean endProgram = true;
 
-      System.out.print("Please enter cash register's float:");
-      s = in.nextLine();
-      balance = Double.parseDouble(s);
+		// comment
+		// Anthony was here.
+		// Test comment 2.
 
-      System.out.print("Please enter the item's name:");
-      s = in.nextLine();
+		Scanner in = new Scanner(System.in);
 
-      System.out.print("Please enter the item's cost:");
-      c = in.nextLine();
+		System.out.print("Please enter cash register's float:");
+		s = in.nextLine();
+		balance = Double.parseDouble(s);
 
-      Transaction trans = new Transaction(s, Double.parseDouble(c));
+		while (endProgram) {
+			
+			System.out.println("Would you like to proceed with the transaction? (y/n)");
+			proceed = in.next().charAt(0);
 
-      System.out.print("Please enter the cash amount tendered:");
-      s = in.nextLine();
-      c = Double.toString(Double.parseDouble(s) - trans.getCost());
+			if (proceed == 'y' || proceed == 'Y') {
 
-      System.out.println("Amount of change required = " + dollar_symbol + c);
+				System.out.print("Please enter the item's name:");
+				s = in.nextLine();
 
-      c = Double.toString(balance + trans.getCost());
+				System.out.print("Please enter the item's cost:");
+				c = in.nextLine();
 
-      System.out.println("Balance of the Cash Register: " + dollar_symbol + c);
+				Transaction trans = new Transaction(s, Double.parseDouble(c));
 
-      //2:13am Test
+				System.out.print("Please enter the cash amount tendered:");
+				s = in.nextLine();
+				c = Double.toString(Double.parseDouble(s) - trans.getCost());
 
-   }
+				System.out.println("Amount of change required = " + dollar_symbol + c);
+
+				c = Double.toString(balance + trans.getCost());
+
+				System.out.println("Balance of the Cash Register: " + dollar_symbol + c);
+
+			}
+
+			else if (proceed == 'n' || proceed == 'N') {
+				System.out.println("Thanks for coming.");
+				endProgram=false;
+			}
+
+			else {
+				System.out.println("Invalid input. Please try again");
+			}
+
+		}
+
+		// 2:13am Test
+
+	}
 }
